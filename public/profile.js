e525fff9cafcbb088dc6aaa38f311fc6f689e8ec
Keheliya-Sathsara <keheliyasm@gmail.com>
2022-02-28 09:07:14 +0530

    Added about.html,profile.html and profile.js

---
+++ public/profile.js
@@ -0,0 +1,13 @@
+import { _app } from "./index.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firetore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-storage.js";
+
+const profile = document.getElementById('profile')
+
+if (profile) {
+    profile.onsubmit = e=>{
+        e.preventDefault()
+        const formData = new FormData(profile);
+        for (let[key,value] of formData) console.log(`${key}==${value}`);
+    }
+}
